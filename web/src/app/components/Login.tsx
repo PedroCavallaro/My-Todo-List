71512f24e73d21a4b23ec1336e4e3557323e8e4a
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-07-07 15:05:31 -0300

    added logout route

--- web/src/app/components/Login.tsx
+++ web/src/app/components/Login.tsx
@@ -2,10 +2,7 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import { User } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { api } from "../lib/api";
-import jwtDecode from "jwt-decode";
-import Cookies from "js-cookie"
-import Image from "next/image";
+
 
 interface userInfo{
     name: string | undefined,
@@ -20,18 +17,8 @@ export default function Login({name, picture, handler}: userInfo) {
         onSuccess: async token => {
             const { access_token } = token
             router.push(`/api/auth?code=${access_token}`)
-            await api.post("/auth", {
-                code: access_token
-            })  
-            .then(({data}) => {
-                // Cookies.set("token", data, {
-                //     expires: 30
-                // })
-                // handler(jwtDecode(data))
-            })
         }
     })     
-    console.log(picture)   
     return(
         <>
         {name ? 
@@ -39,14 +26,11 @@ export default function Login({name, picture, handler}: userInfo) {
             <div className="flex items-center gap-3 text-right mr-5">
                 <div>
                     <p className="">{`Olá ${name}`}</p>
-                    <button className="text-red-400 cursor-pointer hover:text-red-600 transition-all"
-                    onClick={()=> {
-                        Cookies.remove("token")
-                        handler("")
-                    }}
+                    <a className="text-red-400 cursor-pointer hover:text-red-600 transition-all"
+                    href="/api/auth/logout"
                     >
                     Sair
-                    </button>
+                    </a>
                 </div>
                 <div className="flex w-14 h-14 items-center justify-center rounded-full bg-white border-1 border-black overflow-hidden">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
